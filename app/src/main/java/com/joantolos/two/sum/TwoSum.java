4083fcfe71707539df057a203c0357f0211cf712
joantolos <joan.tolos@gmail.com>
2021-09-24 16:31:47 +0200

    Example one test and code placeholder

--- app/src/main/java/com/joantolos/two/sum/TwoSum.java
+++ app/src/main/java/com/joantolos/two/sum/TwoSum.java
@@ -1,4 +1,11 @@
 package com.joantolos.two.sum;
 
 public class TwoSum {
+
+    public int[] run(int[] nums, int target) {
+        int[] solution = new int[2];
+        solution[1] = 1;
+
+        return solution;
+    }
 }
--- app/src/test/java/com/joantolos/two/sum/TwoSumTest.java
+++ app/src/test/java/com/joantolos/two/sum/TwoSumTest.java
@@ -6,8 +6,12 @@ import static org.junit.Assert.*;
 
 public class TwoSumTest {
 
+    TwoSum twoSum = new TwoSum();
+
     @Test
     public void shouldComplyWithExampleOne() {
-        Assert.assertTrue(true);
+        int[] solution = twoSum.run(new int[]{2, 7, 11, 15}, 9);
+        Assert.assertEquals(0, solution[0]);
+        Assert.assertEquals(1, solution[1]);
     }
 }
