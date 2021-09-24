0b23f547b90d8bceb212d18017f9d2c83a90c6f5
joantolos <joan.tolos@gmail.com>
2021-09-24 16:38:29 +0200

    Examples two and three tests added

--- app/src/test/java/com/joantolos/two/sum/TwoSumTest.java
+++ app/src/test/java/com/joantolos/two/sum/TwoSumTest.java
@@ -1,8 +1,8 @@
 package com.joantolos.two.sum;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class TwoSumTest {
 
@@ -14,4 +14,21 @@ public class TwoSumTest {
         Assert.assertEquals(0, solution[0]);
         Assert.assertEquals(1, solution[1]);
     }
+
+    @Test
+    @Ignore
+    public void shouldComplyWithExampleTwo() {
+        int[] solution = twoSum.run(new int[]{3, 2, 4}, 6);
+        Assert.assertEquals(1, solution[0]);
+        Assert.assertEquals(2, solution[1]);
+    }
+
+    @Test
+    @Ignore
+    public void shouldComplyWithExampleThree() {
+        int[] solution = twoSum.run(new int[]{3, 3}, 6);
+        Assert.assertEquals(0, solution[1]);
+        Assert.assertEquals(1, solution[2]);
+    }
+
 }
