ccae85978b71a04d2f8685e5aa97e344139d150c
joantolos <joan.tolos@gmail.com>
2021-09-26 12:13:36 +0200

    Avoiding one extra mutable variable

--- app/src/main/java/com/joantolos/two/sum/TwoSum.java
+++ app/src/main/java/com/joantolos/two/sum/TwoSum.java
@@ -4,18 +4,13 @@ public class TwoSum {
 
     public int[] exponentialAlgorithm(int[] nums, int target) {
         int[] solution = new int[2];
-        boolean found = false;
 
         for (int i = 0; i < nums.length; i++){
-            if (!found) {
-                for (int j = 0; j < nums.length; j++){
-                    if (!found) {
-                        if (i != j && (nums[i] + nums[j] == target)) {
-                            solution[0] = i;
-                            solution[1] = j;
-                            found = true;
-                        }
-                    }
+            for (int j = 0; j < nums.length; j++){
+                if (i != j && (nums[i] + nums[j] == target)) {
+                    solution[0] = i;
+                    solution[1] = j;
+                    return solution;
                 }
             }
         }
@@ -25,18 +20,13 @@ public class TwoSum {
 
     public int[] logarithmAlgorithm(int[] nums, int target) {
         int[] solution = new int[2];
-        boolean found = false;
 
         for (int i = 0; i < nums.length; i++){
-            if (!found) {
-                for (int j = 0; j < nums.length; j++){
-                    if (!found) {
-                        if (i != j && (nums[i] + nums[j] == target)) {
-                            solution[0] = i;
-                            solution[1] = j;
-                            found = true;
-                        }
-                    }
+            for (int j = 0; j < nums.length; j++){
+                if (i != j && (nums[i] + nums[j] == target)) {
+                    solution[0] = i;
+                    solution[1] = j;
+                    return solution;
                 }
             }
         }
