3a405f8c0812f32ba59b006cebf33eaaf97ed882
joantolos <joan.tolos@gmail.com>
2021-09-26 12:15:01 +0200

    Avoiding the last extra mutable variable

--- app/src/main/java/com/joantolos/two/sum/TwoSum.java
+++ app/src/main/java/com/joantolos/two/sum/TwoSum.java
@@ -3,34 +3,26 @@ package com.joantolos.two.sum;
 public class TwoSum {
 
     public int[] exponentialAlgorithm(int[] nums, int target) {
-        int[] solution = new int[2];
-
         for (int i = 0; i < nums.length; i++){
             for (int j = 0; j < nums.length; j++){
                 if (i != j && (nums[i] + nums[j] == target)) {
-                    solution[0] = i;
-                    solution[1] = j;
-                    return solution;
+                    return new int[]{i, j};
                 }
             }
         }
 
-        return solution;
+        return null;
     }
 
     public int[] logarithmAlgorithm(int[] nums, int target) {
-        int[] solution = new int[2];
-
         for (int i = 0; i < nums.length; i++){
             for (int j = 0; j < nums.length; j++){
                 if (i != j && (nums[i] + nums[j] == target)) {
-                    solution[0] = i;
-                    solution[1] = j;
-                    return solution;
+                    return new int[]{i, j};
                 }
             }
         }
 
-        return solution;
+        return null;
     }
 }
